981004655c9f1200693db7ae02b2ec5456271a14
hiroki04y <110616448+hiroki04y@users.noreply.github.com>
2022-11-17 09:39:05 +0900

    11-17 エラー対処

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Allershare
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    onfig.middleware.delete ActionDispatch::HostAuthorization
 
     # Configuration for the application, engines, and railties goes here.
     #
