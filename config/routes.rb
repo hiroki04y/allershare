97cb97a79d708d26c8a35da5efa4ac66ee14eeac
hiroki04y <110616448+hiroki04y@users.noreply.github.com>
2022-11-18 16:11:51 +0900

    11-18 backcolor

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   get '/' => "home#top"
   get '/signup' => "users#signup"
+  
+  
+  get '/blog' => 'blog#blog'
+
+  #ユーザ関連
+  get '/login' => 'users#login'
   get '/users/index' => "users#index"
   get '/users/:id' => 'users#show'
-  get '/login' => 'users#login'
-  get '/blog' => 'blog#blog'
   post '/users/create' => 'users#create'
   get 'users/:id/destroy' => "users#destroy"
+
+  # チャット
   get '/chat' => "chat#chat"
 end
