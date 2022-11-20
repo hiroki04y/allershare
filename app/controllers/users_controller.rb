c319fb2e04f5ce434ed67305a5b3f976b2debe70
hiroki04y <110616448+hiroki04y@users.noreply.github.com>
2022-11-20 17:44:16 +0900

    11-20 ユーザ画像登録完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,13 @@ class UsersController < ApplicationController
         @user.name = params[:name]
         @user.email = params[:email]
         @user.password = params[:password]
+
+        if params[:image]
+            @user.image_name = "#{@user.id}.jpg"
+            image = params[:image]
+            File.binwrite("public/user_images/#{@user.image_name}", image.read)
+        end
+
         if @user.save
             flash[:notice] = "ユーザー情報を編集しました"
             redirect_to("/users/#{@user.id}")
