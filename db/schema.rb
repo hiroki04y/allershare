75d8ea496452096f29fd3a251f22a5f9bf0af965
KatsumataShunsaku <110651431+KatsumataShunsaku@users.noreply.github.com>
2022-11-21 11:53:30 +0900

    11-21-トップ画面変更

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_233419) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
