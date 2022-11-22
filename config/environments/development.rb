13a66b76346cfead3841a0cd79a6a8ec63d16e92
hiroki04y <110616448+hiroki04y@users.noreply.github.com>
2022-11-22 10:34:53 +0900

    11-22 エラー画面2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,8 +55,6 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
-
-  config.consider_all_requests_local = false
   
   # Suppress logger output for asset requests.
   config.assets.quiet = true
@@ -72,4 +70,5 @@ Rails.application.configure do
 
   config.hosts << "allershare.site"
 
+  config.consider_all_requests_local = false
 end
