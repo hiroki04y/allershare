aa9e441edca8a16359ce11e388ebc65136f2f767
hiroki04y <110616448+hiroki04y@users.noreply.github.com>
2022-11-24 02:28:47 +0900

    rm-migrate

--- db/migrate/20221122170655_add_message.rb
+++
@@ -1,9 +0,0 @@
-class AddMessage < ActiveRecord::Migration[7.0]
-  def change
-    create_table :messages do |t|
-      t.text :content
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20221122170831_change_message.rb
+++
@@ -1,7 +0,0 @@
-class ChangeMessage < ActiveRecord::Migration[7.0]
-  def change
-    add_column :messages, :room, :integer, foreign_key: true
-    add_column :messages, :user, :integer, foreign_key: true
-    rename_table :messages, :chat_messages
-  end
-end
