6ec2a53306a2d30401e93ec2d88ffe9efee3aa1f
hiroki04y <110616448+hiroki04y@users.noreply.github.com>
2022-12-02 09:23:17 +0900

    12-2 new

--- app/controllers/message/createchat_controller.rb
+++ app/controllers/message/createchat_controller.rb
@@ -2,7 +2,7 @@ class Message::CreatechatController < ApplicationController
     def index
         @message = ChatMessage.new(content: params[:text], room: params[:room], user_id: params[:user])
         @message.save
-        @chat = ChatMessage.where('room LIKE(?)', "%#{params[:room]}%")
+        @chat = ChatMessage.joins(:user).select('content, room, user_id, name, image_name').where('room LIKE(?)', "%#{params[:room]}%")
         respond_to do |format| # リクエスト形式によって処理を切り分ける
             format.html  # html形式の場合
             format.json { render json: @chat } # json形式の場合
