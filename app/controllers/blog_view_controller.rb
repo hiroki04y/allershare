e29c9fe0e4fa0f9335b4261ead494b947bd40f9f
KatsumataShunsaku <110651431+KatsumataShunsaku@users.noreply.github.com>
2022-12-26 14:25:45 +0900

    12/26 投稿制限機能、画像変換機能

--- app/controllers/blog_view_controller.rb
+++ app/controllers/blog_view_controller.rb
@@ -6,10 +6,23 @@ class BlogViewController < ApplicationController
         @blog = Blog.find_by(id: params[:id])
     end
     def blog_new
+        @blog = Blog.new
     end
     def blog_create
-        @blog = Blog.create(title: params[:title], content: params[:content], image: "blog_default.png")
-        redirect_to("/blog_view")
+        @blog = Blog.create(title: params[:title], content: params[:content])
+        if params[:image]
+            @blog.image = "#{@blog.id}.jpg"
+            image = params[:image]
+            File.binwrite("public/blog_images/#{@blog.image}", image.read)
+        else
+            @blog.image = "blog_default.png"
+        end
+        if @blog.save
+            flash[:notice] = "投稿を作成しました"
+            redirect_to("/blog_view")
+        else
+            render("/blog_view/blog_new")
+        end
     end
     def blog_edit
         @blog = Blog.find_by(id: params[:id])
@@ -18,12 +31,18 @@ class BlogViewController < ApplicationController
         @blog = Blog.find_by(id: params[:id])
         @blog.title = params[:title]
         @blog.content = params[:content]
-        @blog.save
-        redirect_to("/blog_view")
+        @blog.image = params[:image]
+        if @blog.save
+            flash[:notice] = "投稿を編集しました"
+            redirect_to("/blog_view")
+        else
+            render("/blog_view/blog_edit")
+        end
     end
     def blog_destroy
         @blog = Blog.find_by(id: params[:id])
-        @blog.destroy    
+        @blog.destroy
+        flash[:notice] = "投稿を削除しました"    
         redirect_to("/blog_view")
     end
 end
