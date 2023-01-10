3599c96c8c6f2722c38bff5d5671fde53877cb81
hiroki04y <hiroki02y104@icloud.com>
2023-01-10 15:22:36 +0900

    1-10 personalchat表示完了

--- app/controllers/message/sendchat_controller.rb
+++ app/controllers/message/sendchat_controller.rb
@@ -1,13 +1,13 @@
 class Message::SendchatController < ApplicationController
     def index
         if(params[:id] == "1")
-            @chat = ChatMessage.joins(:user).select('content, room, user_id, name, image_name').where('room LIKE(?)', "%#{params[:room]}%")
+            @chat = ChatMessage.joins(:user).select('content, room, user_id, name, image_name').where('room LIKE(?)', "#{params[:room]}")
             respond_to do |format| # リクエスト形式によって処理を切り分ける
                 format.html  # html形式の場合
                 format.json { render json: @chat } # json形式の場合
             end
         else
-            @chat = User.all
+            @chat = Personalchat.joins(:user).select('content, send_id, user_id, name, image_name').where('send_id = ? and user_id = ? or send_id = ? and user_id = ?' , "#{params[:usr]}", "#{params[:pair]}", "#{params[:pair]}", "#{params[:usr]}")
             respond_to do |format| # リクエスト形式によって処理を切り分ける
                 format.html  # html形式の場合
                 format.json { render json: @chat } # json形式の場合
