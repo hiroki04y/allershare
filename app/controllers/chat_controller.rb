ac049e6e02f40e6cbed7ea0d36f556132856653b
hiroki04y <hiroki02y104@icloud.com>
2023-01-10 11:13:25 +0900

    1-10 personalchat 作成中

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,5 +3,6 @@ class ChatController < ApplicationController
         @room = Chatroom.all
         @user = User.all
         @top = ChatMessage.where('room LIKE(?)', "0")
+        @following_users = @current_user.following_user
     end
 end
--- app/controllers/message/search_controller.rb
+++ app/controllers/message/search_controller.rb
@@ -7,10 +7,10 @@ class Message::SearchController < ApplicationController
                 format.json { render json: @room } # json形式の場合
             end
         else
-            @room = User.where('name LIKE(?)', "%#{params[:roomname]}%")
+            @following_users = @current_user.following_user
             respond_to do |format| # リクエスト形式によって処理を切り分ける
                 format.html  # html形式の場合
-                format.json { render json: @room } # json形式の場合
+                format.json { render json: @following_users } # json形式の場合
             end
         end
     end
