46351055df764e0800a41d5558247885e9af0d0c
hiroki04y <hiroki02y104@icloud.com>
2023-01-11 16:47:23 +0900

    1-11 personalchatエラーⅡ

--- app/controllers/message/sendchat_controller.rb
+++ app/controllers/message/sendchat_controller.rb
@@ -7,7 +7,7 @@ class Message::SendchatController < ApplicationController
                 format.json { render json: @chat } # json形式の場合
             end
         else
-            @chat = Personalchat.joins(:user).select('content, send_id, user_id, name, image_name').where('send_id = ? and user_id = ? or send_id = ? and user_id = ?' , "#{params[:usr]}", "#{params[:pair]}", "#{params[:pair]}", "#{params[:usr]}").order(id: "DESC")
+            @chat = Personalchat.joins(:user).select('content, send_id, user_id, name, image_name').where('send_id = ? and user_id = ? or send_id = ? and user_id = ?' , "#{params[:usr]}", "#{params[:pair]}", "#{params[:pair]}", "#{params[:usr]}").order(id: "ASC")
             respond_to do |format| # リクエスト形式によって処理を切り分ける
                 format.html  # html形式の場合
                 format.json { render json: @chat } # json形式の場合
